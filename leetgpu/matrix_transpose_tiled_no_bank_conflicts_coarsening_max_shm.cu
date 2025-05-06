736812b679f49a7e9e0297317d4d8b25f0e027a7
Gordon Gustafson <gordon3.14@gmail.com>
2025-05-06 12:10:19 -0700

    Transpose kernel with max shared memory

---
+++ leetgpu/matrix_transpose_tiled_no_bank_conflicts_coarsening_max_shm.cu
@@ -0,0 +1,46 @@
+#include "solve.h"
+#include <cuda_runtime.h>
+
+#define CEIL_DIV(dividend, divisor) (((dividend) + (divisor) - 1) / (divisor))
+
+#define BLOCK_SIZE 32
+// There's 64 of dynamic shared memory allowed per SM in a T4 GPU.
+// We can't fit a value of 16 due to the BLOCK_SIZE+1 that avoids shared memory bank conflicts.
+#define COARSENING_FACTOR 15
+
+__global__ void matrix_transpose_kernel(const float* input, float* output, int inputRows, int inputCols) {
+    // __shared__ float blockShared[COARSENING_FACTOR*BLOCK_SIZE][BLOCK_SIZE+1];
+    __shared__ extern float blockShared[];
+    int const colsInBlockShared = BLOCK_SIZE + 1;
+    int const inputCol = blockIdx.x * blockDim.x + threadIdx.x;
+    int const inputRow = blockIdx.y * blockDim.y * COARSENING_FACTOR + threadIdx.y;
+    #pragma unroll
+    for (int i = 0; i < COARSENING_FACTOR; i++) {
+        bool const inputInBounds = inputCol < inputCols && (i * blockDim.y + inputRow) < inputRows;
+        if (inputInBounds) {
+            blockShared[(i * blockDim.y + threadIdx.y) * colsInBlockShared + threadIdx.x] = input[(i * blockDim.y + inputRow) * inputCols + inputCol];
+        }
+    }
+
+    __syncthreads();
+
+    int const outputCol = blockIdx.y * blockDim.y * COARSENING_FACTOR + threadIdx.x;
+    int const outputRow = blockIdx.x * blockDim.x + threadIdx.y;
+    #pragma unroll
+    for (int i = 0; i < COARSENING_FACTOR; i++) {
+        bool const outputInBounds = outputCol + i * blockDim.y < inputRows && outputRow < inputCols;
+        if (outputInBounds) {
+            output[outputRow * inputRows + outputCol + i * blockDim.y] = blockShared[(threadIdx.x + i * blockDim.y) * colsInBlockShared + threadIdx.y];
+        }
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+void solve(const float* input, float* output, int rows, int cols) {
+    dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 blocksPerGrid(CEIL_DIV(cols, BLOCK_SIZE),
+                       CEIL_DIV(rows, BLOCK_SIZE * COARSENING_FACTOR));
+    int sharedMemory = COARSENING_FACTOR * BLOCK_SIZE * (BLOCK_SIZE+1) * sizeof(float);
+    cudaFuncSetAttribute(matrix_transpose_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, sharedMemory);
+    matrix_transpose_kernel<<<blocksPerGrid, threadsPerBlock, sharedMemory>>>(input, output, rows, cols);
+}
