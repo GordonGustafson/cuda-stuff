456389b61ef88074a6edff639443d933c9bc3d2a
Gordon Gustafson <gordon3.14@gmail.com>
2025-05-13 17:18:23 -0700

    More scratch WIP on flash attention

--- leetgpu/softmax_attention_flash_attention.cu
+++ leetgpu/softmax_attention_flash_attention.cu
@@ -14,12 +14,19 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 #define CEIL_DIV(dividend, divisor) (((dividend) + (divisor) - 1) / (divisor))
 
-int getMaxSharedMemPerBlockOptin() {
-    int device;
-    cudaGetDevice(&device);
-    cudaDeviceProp props;
-    cudaGetDeviceProperties(&props, device);
-    return props.sharedMemPerBlockOptin;
+__device__ static inline float onlineSoftmaxSum(float const maxA,
+                                                float const sumA,
+                                                float const maxB,
+                                                float const sumB) {
+    if (sumA == 0.0f) {
+        return sumB;
+    } else if (sumB == 0.0f) {
+        return sumA;
+    } else if (maxA > maxB) {
+        return sumB * expf(maxB - maxA) + sumA;
+    } else {
+        return sumB + sumA * expf(maxA - maxB);
+    }
 }
 
 __global__ void flash_attention_kernel(float const* const Q_HBM,  // size Mxd
@@ -29,6 +36,8 @@ __global__ void flash_attention_kernel(float const* const Q_HBM,  // size Mxd
                                        int const M,
                                        int const N,
                                        int const d,
+                                       float* const row_sum_HBM;
+                                       float* const row_max_HBM;
                                        int const maxSharedMemory) {
     extern __shared__ float sharedMemory[];
     int const B_c = CEIL_DIV(maxSharedMemory, 4*d);
@@ -41,31 +50,53 @@ __global__ void flash_attention_kernel(float const* const Q_HBM,  // size Mxd
     float* const V = K + B_c * d;
     float* const S = V + B_c * d;
 
-    float l_i = 0.0f;
-    float m_i = -INFINITY;
-
+    // Initialize S
     for (int B_r_index = 0; B_r_index < B_r; B_r_index++) {
-        S[B_r_index * d + threadIdx.x] = 0.0f;
+        S[B_r_index * B_c + threadIdx.x] = 0.0f;
     }
 
-    // Load V and K.
-    for (int B_c_index = 0; B_c_index < B_c; B_c_index++) {
-        V[B_c_index * d + threadIdx.x] = V_HBM[blockIdx.x * d * B_c + B_c_index * d + threadIdx.x];
-        K[B_c_index * d + threadIdx.x] = K_HBM[blockIdx.x * d * B_c + B_c_index * d + threadIdx.x];
+    // Load Q
+    for (int d_index = 0; d_index < d; d_index++) {
+        if (threadIdx.x < B_r) {
+            Q[threadIdx.x * d + d_index] = Q_HBM[blockIdx.x * d * B_r + threadIdx.x * d + d_index];
+        }
     }
 
-    // Load Q.
-    for (int B_r_index = 0; B_r_index < B_r; B_r_index++) {
-        Q[B_r_index * d + threadIdx.x] = V_HBM[blockIdx.y * d * B_r + B_r_index * d + threadIdx.x];
-    }
+    // Iterate horizontally different S blocks.
+    for (int T_c_index = 0; T_c_index < T_c; T_c_index++) {
+        // Load K and V
+        for (int d_index = 0; d_index < d; d_index++) {
+            K[threadIdx.x * d + d_index] = K_HBM[T_c_index * d * B_c + threadIdx.x * d + d_index];
+            V[threadIdx.x * d + d_index] = V_HBM[T_c_index * d * B_c + threadIdx.x * d + d_index];
+        }
 
-    for (int B_c_index = 0; B_c_index < B_c; B_c_index++) {
-        for (int B_r_index = 0; B_r_index < B_c; B_r_index++) {
+        __syncthreads();
+
+        // Iterate vertically within the S block.
+        for (int B_r_index = 0; B_r_index < B_r; B_r_index++) {
+            float S_row_max = -INFINITY;
+            float S_row_sum = 0.0f
             float result = 0.0f;
             for (int d_index = 0; d_index < d; d_index++) {
-                result += Q[B_r_index * d + d_index] * K[B_r_index * d + d_index];
+                result += Q[B_r_index * d + d_index] * K[threadIdx.x * d + d_index];
             }
-            S[B_r_index * B_c + B_c_index] = result;
+            S[B_r_index * B_c + threadIdx.x] = result;
+            __syncthreads();
+
+            // Update max and sum for this row.
+            if (threadIdx.x == 0) {
+                for (int col = 0; col < B_c; col++) {
+                    float const S_val = S[B_r_index * B_c + col];
+                    S_row_sum = onlineSoftmaxSum(S_row_max, S_row_Sum, S_val, 1.0f);
+                    S_row_max = std::max(S_row_max, S_val);
+                }
+                int const row_index = blockIdx * B_r + B_r_index;
+                row_sum_HBM[row_index] = onlineSoftmaxSum(row_max_HBM[row_index],
+                                                        row_sum_HBM[row_index],
+                                                        S_row_max,
+                                                        S_row_sum);
+                row_max_HBM[row_index] = std::max(row_max_HBM[row_index], S_row_max);
+            }
         }
     }
 }
@@ -80,7 +111,8 @@ void solve(float const* const Q,  // size Mxd
            int const M,
            int const N,
            int const d) {
-    int const maxSharedMemory = getMaxSharedMemPerBlockOptin();
+    int maxSharedMemory;
+    cudaDeviceGetAttribute(&maxSharedMemory, cudaDevAttrMaxSharedMemoryPerBlock, 0);
     cudaFuncSetAttribute(flash_attention_kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, maxSharedMemory);
 
     int const B_c = CEIL_DIV(maxSharedMemory, 4*d);
@@ -92,8 +124,8 @@ void solve(float const* const Q,  // size Mxd
     float* zeroFloats = new float[M*d]();
     cudaMemcpy(output, zeroFloats, M * d * sizeof(float), cudaMemcpyHostToDevice);
 
-    dim3 const blocksPerGrid(T_c, T_r);
-    dim3 const threadsPerBlock(B_c, B_r);
+    dim3 const blocksPerGrid(T_r);
+    dim3 const threadsPerBlock(B_c);
     flash_attention_kernel<<<blocksPerGrid, threadsPerBlock, maxSharedMemory>>>(Q, K, V, output, M, N, d, maxSharedMemory);
 }
 
